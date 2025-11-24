8ec9b20c8fed41f8d8a1e0024a62e4262fd19fd3
thoufik1111 <mohammedthoufik22@gmail.com>
2025-11-24 21:23:37 +0530

    Fixed alias + toaster + GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
-  base: "/AUTICARE.in/",  // REQUIRED for GitHub Pages
+  base: "AUTICARE.in/",
   plugins: [react()],
-  build: {
-    outDir: "dist",
-    sourcemap: false,
-  },
   resolve: {
     alias: {
-      "@/": "/src/",
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
